daaf95688e90b2842f8f0419416866b75b97e721
harubex <hannah@harubex.com>
2018-01-24 23:32:12 -0700

    Added check for two-faced cards.

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -11,13 +11,14 @@ class Cards extends React.Component {
         return (
             <div className={`${classes.card_container} container`}>
                 {this.props.context.cardData.map((card) => (
+                    
                     <CardGlance key={uuid()} 
                         collectionId={this.props.context.collectionId} 
                         name={card.name}
                         normal_qty={card.normal_qty} 
                         foil_qty={card.foil_qty} 
                         num={card.collector_number}
-                        image_uri={card.card_faces ? card.card_faces[0].image_uris.small : (
+                        image_uri={(card.card_faces && !card.image_uris) ? card.card_faces[0].image_uris.small : (
                             card.image_uris ? card.image_uris.small : card.image_uri
                         )}
                         usd={card.usd}
